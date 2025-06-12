f5a4601f0eaa629554e4e8072f3fb818973405f8
error <70842643+errorcat404@users.noreply.github.com>
2025-06-12 19:16:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
     // Карта текстов и соответствующих ссылок
-  const linkMap = {
-    "Work": "https://1kittyka1.github.io/work2grow./",
-    "Build": "https://build.com",
-    "Read": "https://errorcat404.github.io/read2grow/",
-    "Mistakes": "https://shavuha112.github.io/M2G/",
-    "Grow": "https://sad12knof.github.io/grow2grow/",
-    "Work hard": "https://kst918.github.io/work-hard2grow/",
-    "Coding": "https://arsdim.github.io/Test/",
-    "Game": "https://vuiacheslav.github.io/game_to_grow/",
-    "Act": "https://vov4ik23.github.io/act2grow-/",
-    "Dream": "https://neswwex.github.io/Live2Grow/",
-    "Think": "https://godlik48.github.io/Think2Grow/",
-    "Live": "https://neswwex.github.io/Live2Grow/",
-    "Sport": "yarikzxcvbnzxcvbn.github.io/sport2grow/"
-  };
+const linkMap = {
+  "Work": "https://1kittyka1.github.io/work2grow/",
+  "Build": "https://build.com",
+  "Read": "https://errorcat404.github.io/read2grow/",
+  "Mistakes": "https://shavuha112.github.io/M2G/",
+  "Grow": "https://sad12knof.github.io/grow2grow/",
+  "Work hard": "https://kst918.github.io/work-hard2grow/",
+  "Coding": "https://arsdim.github.io/Test/",
+  "Game": "https://vuiacheslav.github.io/game_to_grow/",
+  "Act": "https://vov4ik23.github.io/act2grow-/",
+  "Dream": "https://neswwex.github.io/Live2Grow/",
+  "Think": "https://godlik48.github.io/Think2Grow/",
+  "Live": "https://neswwex.github.io/Live2Grow/",
+  "Sport": "https://yarikzxcvbnzxcvbn.github.io/sport2grow/"
+};
 
   // Ищем все элементы .dot-label внутри .side-labels
   document.querySelectorAll('.side-labels .dot-label').forEach(label => {
