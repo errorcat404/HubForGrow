c49a228ea70d330c1367e27dcda3e5fd92afb27c
error <70842643+errorcat404@users.noreply.github.com>
2025-06-17 19:04:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const linkMap = {
   "Build": "https://sodovaya-source.github.io/gus333/",
   "Read": "https://errorcat404.github.io/read2grow/",
   "Mistakes": "https://shavuha112.github.io/M2G/",
-  "Grow": "https://sad12knof.github.io/grow2grow/",
+  "Grow1": "https://sad12knof.github.io/grow2grow/",
+  "Grow2": "https://hamgaa.github.io/examenlyakhxx/",
   "Work hard": "https://kst918.github.io/work-hard2grow/",
   "Coding": "https://alexmach0109.github.io/coding.github.io/",
   "Game": "https://vuiacheslav.github.io/game_to_grow/",
   "Act": "https://vov4ik23.github.io/act2grow-/",
   "Dream": "https://nastygrusha31.github.io/Dream2Grow/",
-  "Think": "https://godlik48.github.io/Think2Grow/",
+  "Think": "https://godlik48.github.io/Think_2_Grow/",
   "Live": "https://neswwex.github.io/Live2Grow/",
   "Sport": "https://yarikzxcvbnzxcvbn.github.io/sport2grow/"
 };
